0350d4747e7ddc758b75273ce624c8c4458ea5ee
Germán Méndez Bravo <kronuz@fb.com>
2022-07-21 10:41:17 -0700

    Add `lazy_imports` to `sys.flags`
    Summary:
    This adds `lazy_imports` to `sys.flags` and sets it to `True` when
    the `-L` flag is passed to the interpreter.
    
    `importlib.set_lazy_imports()` doesn't have any effect in this flag;
    to get the current status of lazy imports and see whether is enabled,
    use `importlib.is_lazy_imports_enabled()` instead.
    
    Test Plan:
    
    ```
    $ ./python -L -c 'import sys; print(sys.flags)'
    sys.flags(debug=0, inspect=0, interactive=0, optimize=0, dont_write_bytecode=0, no_user_site=0, no_site=0, ignore_environment=0, verbose=0, bytes_warning=0, quiet=0, hash_randomization=1, isolated=0, dev_mode=False, utf8_mode=0, warn_default_encoding=0, safe_path=False, lazy_imports=True)
    ```

--- Python/sysmodule.c
+++ Python/sysmodule.c
@@ -2547,7 +2547,8 @@ static PyStructSequence_Field flags_fields[] = {
     {"dev_mode",                "-X dev"},
     {"utf8_mode",               "-X utf8"},
     {"warn_default_encoding",   "-X warn_default_encoding"},
-    {"safe_path", "-P"},
+    {"safe_path",               "-P"},
+    {"lazy_imports",            "-L"},
     {0}
 };
 
@@ -2555,7 +2556,7 @@ static PyStructSequence_Desc flags_desc = {
     "sys.flags",        /* name */
     flags__doc__,       /* doc */
     flags_fields,       /* fields */
-    17
+    18
 };
 
 static int
@@ -2596,6 +2597,7 @@ set_flags_from_config(PyInterpreterState *interp, PyObject *flags)
     SetFlag(preconfig->utf8_mode);
     SetFlag(config->warn_default_encoding);
     SetFlagObj(PyBool_FromLong(config->safe_path));
+    SetFlagObj(PyBool_FromLong(config->lazy_imports));
 #undef SetFlagObj
 #undef SetFlag
     return 0;
