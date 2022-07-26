224c74ea19708e8db434c471a4c15947c4398357
Germán Méndez Bravo <kronuz@fb.com>
2022-07-26 13:55:43 -0700

    Fixing lazyimports tests

--- Lib/test/lazyimports/dict_update.py
+++ Lib/test/lazyimports/dict_update.py
@@ -1,9 +1,7 @@
-# TODO enable lazy imports tests and FIXME
-# from __future__ import lazy_imports
-
 import warnings
 
 vars = {}
 vars.update(globals())
-print(repr(vars['warnings']))
+
+result = vars['warnings']
 
