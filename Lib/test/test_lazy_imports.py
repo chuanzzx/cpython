9b2316a7599ec6840a9ceb114bc405d08ec8547c
Germán Méndez Bravo <kronuz@fb.com>
2022-07-26 13:55:43 -0700

    Fixing lazyimports tests

--- Lib/test/test_lazy_imports.py
+++ Lib/test/test_lazy_imports.py
@@ -1,6 +1,10 @@
 import importlib
 import unittest
+import warnings
+
 from test.support.script_helper import run_python_until_end
+from test.support.import_helper import import_fresh_module
+
 
 class TestLazyImportsSanity(unittest.TestCase):
     def __init__(self, *args, **kwargs):
@@ -15,67 +19,49 @@ class TestLazyImportsSanity(unittest.TestCase):
         self.assertFalse(importlib.is_lazy_imports_enabled())
 
 class LazyImportsTest(unittest.TestCase):
-    def assertStartswith(self, str1, str2):
-        self.assertTrue(str1.startswith(str2), f"{str1!r}.startswith({str2!r})")
-
-    def assertNotStartswith(self, str1, str2):
-        self.assertFalse(str1.startswith(str2), f"not {str1!r}.startswith({str2!r})")
-
-    def python_run(self, m, lazy=True):
-        args = []
-        if lazy:
-            args += [
-                "-L"
-            ]
-
-        args += [
-            "-m",
-            m,
-        ]
-        res, cmd_line = run_python_until_end(*args)
-        return (
-            res.rc,
-            res.out.decode("ascii", "replace").rstrip(),
-            res.err.decode("ascii", "replace").rstrip(),
-        )
-
-    def run_and_check(self, module, lazy_imports=True, expected_rc=0, expected_out="", expected_err=""):
-        rc, out, err = self.python_run(module, lazy=lazy_imports)
-        self.assertEqual(rc, expected_rc)
-        self.assertEqual(out, expected_out)
-        self.assertEqual(err, expected_err)
-
     def test_dict_update(self):
-        rc, out, err = self.python_run("test.lazyimports.dict_update")
-        self.assertNotStartswith(out, "<deferred ")
-        self.assertStartswith(out, "<module 'warnings' from ")
+        with importlib.lazy_imports(True):
+            mod = import_fresh_module("test.lazyimports.dict_update")
+            self.assertEqual(mod.result, warnings)
 
     def test_deferred_resolve_failure(self):
-        rc, out, err = self.python_run("test.lazyimports.deferred_resolve_failure")
-        self.assertStartswith(out, "<function type_from_ast at ")
+        with importlib.lazy_imports(True):
+            import_fresh_module("test.lazyimports.deferred_resolve_failure")
 
-        rc, out, err = self.python_run("test.lazyimports.deferred_resolve_failure", lazy=False)
-        self.assertNotEqual(rc, 0)
-        self.assertIn("(most likely due to a circular import)", err)
+        with importlib.lazy_imports(False):
+            with self.assertRaises(ImportError) as cm:
+                import_fresh_module("test.lazyimports.deferred_resolve_failure")
+            ex = cm.exception
+            self.assertIn("(most likely due to a circular import)", repr(ex))
 
     def test_split_fromlist(self):
-        correct_out = "['test.lazyimports.split_fromlist.foo', 'test.lazyimports.split_fromlist.foo.bar']"
-        self.run_and_check("test.lazyimports.split_fromlist", expected_out=correct_out)
+        expected_result = {'test.lazyimports.split_fromlist.foo', 'test.lazyimports.split_fromlist.foo.bar'}
+        with importlib.lazy_imports(True):
+            mod = import_fresh_module("test.lazyimports.split_fromlist")
+            self.assertEqual(mod.result, expected_result)
 
     def test_enable_lazy_imports_at_runtime(self):
-        self.run_and_check("test.lazyimports.importlib_apis.enable_lazy_imports_at_runtime", False)
-
-    def test_set_lazy_imports_excluding(self):
-        self.run_and_check("test.lazyimports.importlib_apis.set_lazy_imports_excluding_list", False)
-        self.run_and_check("test.lazyimports.importlib_apis.set_lazy_imports_excluding_cb", False)
-        self.run_and_check("test.lazyimports.importlib_apis.set_lazy_imports_excluding_cb_list", False)
-        self.run_and_check("test.lazyimports.importlib_apis.set_lazy_imports_excluding_list")
-        self.run_and_check("test.lazyimports.importlib_apis.set_lazy_imports_excluding_cb")
-        self.run_and_check("test.lazyimports.importlib_apis.set_lazy_imports_excluding_cb_list")
+        with importlib.lazy_imports(False):
+            import_fresh_module("test.lazyimports.importlib_apis.enable_lazy_imports_at_runtime")
 
     def test_attribute_side_effect(self):
-        self.run_and_check("test.lazyimports.attr_side_effect", False)
-        self.run_and_check("test.lazyimports.attr_side_effect")
+        with importlib.lazy_imports(False):
+            import_fresh_module("test.lazyimports.attr_side_effect")
+
+        with importlib.lazy_imports(True):
+            import_fresh_module("test.lazyimports.attr_side_effect")
+
+    def test_set_lazy_imports_excluding(self):
+        with importlib.lazy_imports(False):
+            import_fresh_module("test.lazyimports.importlib_apis.set_lazy_imports_excluding_list")
+            import_fresh_module("test.lazyimports.importlib_apis.set_lazy_imports_excluding_cb")
+            import_fresh_module("test.lazyimports.importlib_apis.set_lazy_imports_excluding_cb_list")
+
+        with importlib.lazy_imports(True):
+            import_fresh_module("test.lazyimports.importlib_apis.set_lazy_imports_excluding_list")
+            import_fresh_module("test.lazyimports.importlib_apis.set_lazy_imports_excluding_cb")
+            import_fresh_module("test.lazyimports.importlib_apis.set_lazy_imports_excluding_cb_list")
+
 
 if __name__ == '__main__':
     unittest.main()
